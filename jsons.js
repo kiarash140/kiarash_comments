bb99f000a1b94a044437a9ee673c8786e9fbb586
kiarash140 <22527887kiarash@gmail.com>
2025-09-13 14:56:28 +0330

    Initial commit

---
+++ jsons.js
@@ -0,0 +1,43 @@
+let people=[
+    {
+       "onvan": "#mac_donalds",
+        "matn": "It's a real shame that my local #mcdonald's removed milkshakes from the menu. they were the reason I go to mcdonald's. 😩 please bring them back!",
+        "like": 4,
+
+    },
+    {
+        "onvan": "#amazon",
+        "matn": "Im an #amazon prime member but don't really watch the prime video offering. instead of that I would want an option for even faster delivery 😎",
+        "like": 4,
+    },
+    {
+        "onvan": "#netflix",
+        "matn": "since yday on mobile #netflix keeps buffering the video, it keeps happening even when I redownload the app. I'm in an area with decent internet btw",
+        "like": 4,
+    },
+    {
+        "onvan": "#nike",
+        "matn": "hi #nike I love your running shoes but it's very difficult to return them after a purchase. had to do a lot of phone calls to make it work. thanks 👍",
+        "like": 4,
+    },
+    {
+        "onvan": "#star_bucks",
+        "matn": "I really wish #Starbucks would use hand wrappers for hot drinks as a standard, I keep burning my hands and am tired of bothering the employee.",
+        "like": 4,
+    },
+    {
+        "onvan": "#microsoft",
+        "matn": "i've been using #microsoft teams for a couple weeks now and 1 thing that really sticks out is that navigation is too difficult. please simplify it.",
+        "like": 4,
+    },
+    {
+        "onvan": "#netflix",
+        "matn": "would be great if #netflix could announce content removals further ahead. 😊 I dont want to get into a show only for it to be gone soon. thanks",
+        "like": 4,
+    },
+    {
+        "onvan": "#amazon",
+        "matn": "Hi #amazon, I really like the courses. 😁 I just wish that you would produce more of them and faster. That would be great, as I want to be a dev!",
+        "like": 4,
+    },
+]
